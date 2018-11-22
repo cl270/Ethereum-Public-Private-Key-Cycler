87a8d47f56d5593dd788c2ed8ec231d25cda3eba
Paul <cl270@duke.edu>
2018-11-22 16:51:19 -0500

    Privkey edge case

--- src/ETHAddressPubPrivKeyCycler.java
+++ src/ETHAddressPubPrivKeyCycler.java
@@ -4,7 +4,9 @@ public class ETHAddressPubPrivKeyCycler {
 	    {
 	        String privkey = "";
 	        
+	        String digitHex;
 	        int digit;
+	        int fcounter = 0;
 	        
 	        //64 hex characters in an Ethereum private Key, with the constraint that it must 
 	        //also be a valid value on the secp256k1 curve
@@ -13,9 +15,26 @@ public class ETHAddressPubPrivKeyCycler {
 	        //to
 	        //fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141
 	        for(int i = 0; i<64; i++) {
+	        	//edge case that f is (pseudo)randomly chosen 31 times in a row to 
+	        	if(i == 31 && fcounter == 31) {  
+	        		privkey = completer(privkey);
+	        		break;
+	        	}
+	        	
 	        	digit = ThreadLocalRandom.current().nextInt(0, 16);
-	        	privkey += Integer.toHexString(digit);
+	        	digitHex = Integer.toHexString(digit);
+	        	privkey += digitHex;
+	        	
+	        	if(digitHex.equals("f")) fcounter++;
 	        }
-
+	        
+	        //print privkey, and next 100 private keys in sequence, 
+	        
+	    }
+	    
+	    //complete edge case private key
+	    private static String completer (String incompletePrivKey) {
+	        String maximum = "ebaaedce6af48a03bbfd25e8cd0364141";
+	        return "";        
 	    }
 }
